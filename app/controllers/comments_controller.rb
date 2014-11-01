cf35af856bfe7b4d6af4eb34f85175cdaa68c0cd
kkammo <rland14@naver.com>
2014-11-01 22:10:52 +0900

    modify Comment Controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+
+  def create
+    @project = Project.find(params[:project_id])
+    @user = current_user
+    @comment = @post.comments.create(params[:comment])
+    redirect_to post_path(@post)
+  end
+
 end
