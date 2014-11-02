29b087031a8b1059f2fe9ec43893d1288e56dd63
kwony <chkwon91@gmail.com>
2014-11-02 17:10:15 +0900

    Update directories_controller.rb

--- app/controllers/directories_controller.rb
+++ app/controllers/directories_controller.rb
@@ -1,18 +1,23 @@
 class DirectoriesController < ApplicationController
   before_action :set_directory, only: [:show, :edit, :update, :destroy]
-
+#checking!!!
   def index
     @directories = Directory.all
-    respond_with(@directories)
+#    respond_with(@directories)
   end
 
   def show
-    respond_with(@directory)
+    @directory = Directory.find(params[:id])
+
+    respond_to do |format|
+      format.html #show.html.erb
+      format.xml { render :xml => @directory }
+    end
   end
 
   def new
     @directory = Directory.new
-    respond_with(@directory)
+    #respond_with(@directory)
   end
 
   def edit
@@ -21,17 +26,25 @@ class DirectoriesController < ApplicationController
   def create
     @directory = Directory.new(directory_params)
     @directory.save
-    respond_with(@directory)
+    #respond_with(@directory)    
+
+    respond_to do |format|
+      if @directory.save
+        format.html { redirect_to directory_path(@directory), notice: 'Directory created'}
+      else
+        format.html { render action: "new" }
+      end
+    end
   end
 
   def update
     @directory.update(directory_params)
-    respond_with(@directory)
+    #respond_with(@directory)
   end
 
   def destroy
     @directory.destroy
-    respond_with(@directory)
+    #respond_with(@directory)
   end
 
   private
