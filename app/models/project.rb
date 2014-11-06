053ed9ea95b6c792e614ee829ccf92d679cea447
ChangHee <leechhe90@gmail.com>
2014-11-06 11:17:19 +0900

    project belongs to their parent and has many children

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,10 +2,11 @@ class Project < ActiveRecord::Base
 	belongs_to :directory
 	belongs_to :user
 	belongs_to :directory_homework
+	belongs_to :project
+	has_many :projects
 
 	def self.search(query)
-    # where(:title, query) -> This would return an exact match of the query
-    where("project_name like ?", "%#{query}%")
-  end
-
+	    # where(:title, query) -> This would return an exact match of the query
+	    where("project_name like ?", "%#{query}%")
+	end
 end
