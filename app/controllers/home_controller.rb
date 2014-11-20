7a1c4ca960cbfc631f7dc5801bba65828fe820b2
kwony <chkwon91@gmail.com>
2014-11-20 02:28:29 -0800

    관리자가 승인하지 않은 상태로 로그인 할 때 관리

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
 
   def index
-  	if current_user
+  	if current_user.permit
   		require_permit
   		@directory_semesters = DirectorySemester.all
   		@projects = Project.where(user_id:current_user.id).all if Project.all.count > 0
-   	end  	
+   	end
   end
 end
