8678a0b5335f7b4728cfdde2535134be7506fc40
kwony <chkwon91@gmail.com>
2014-11-20 02:42:31 -0800

    log out error fixed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-  	if current_user.permit
+  	if current_user
   		require_permit
   		@directory_semesters = DirectorySemester.all
   		@projects = Project.where(user_id:current_user.id).all if Project.all.count > 0
