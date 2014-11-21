a318929ff3bebbe431a1217f00becb3660ccc7e0
ChangHee <leechhe@ChangHeeui-MacBook-Pro.local>
2014-11-21 21:23:15 +0900

    수정 시 기존 제목이 Default 값으로 설

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,7 @@ Rails.application.routes.draw do
 
   resources :projects do
     get 'search', on: :collection
-    get "projectall", on: :collection
-    get "project_show"
-    get "project_edit"
-    delete "project_destroy"
-
+  
     resources :comments
     resources :likes
   end
