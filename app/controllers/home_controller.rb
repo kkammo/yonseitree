5acce53d58c4b1e95f33b75abddaa8a10af9c3b5
ChangHee <leechhe@ChangHeeui-MacBook-Pro.local>
2014-11-21 17:17:39 +0900

    생성한 프로젝트가 없을 때, pagination 오류 수정

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ class HomeController < ApplicationController
   			require_permit	
   			@directory_semesters = DirectorySemester.all
   			@projects = Project.where(user_id:current_user.id).all if Project.all.count > 0
-  			@projects = Kaminari.paginate_array(@projects).page(params[:page]).per(5)
+  			@projects = Kaminari.paginate_array(@projects).page(params[:page]).per(5) unless @projects.nil?
   		end
    	end  	
   end
