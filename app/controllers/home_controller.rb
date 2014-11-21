b9f234ff769252ebbda58b3685d882efb7226d26
kwony <chkwon91@gmail.com>
2014-11-20 23:46:35 -0800

    회원가입 에러 수정

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-  	if current_user
+  	if current_user.permit
   		require_permit
   		@directory_semesters = DirectorySemester.all
   		@projects = Project.where(user_id:current_user.id).all if Project.all.count > 0
