02e064324efe6a44442c49af78a4ce0e0b31b8fd
Soonjo Jin <soonjoj@Soonjos-MacBook-Air.local>
2014-11-21 11:27:18 +0900

    we can go to back when we create like, commentsgygy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
-class HomeController < ApplicationController
+ class HomeController < ApplicationController
 
   def index
   	if current_user
   		require_permit
   		@directory_semesters = DirectorySemester.all
-  		@projects = Project.where(user_id:current_user.id).all
+  		@projects = Project.where(user_id: current_user.id).all
    	end  	
   end
 end
