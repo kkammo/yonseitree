50390a1e64f092f2f00ffea188251ba0dceb7b08
ChangHee <leechhe@ChangHeeui-MacBook-Pro.local>
2014-11-21 12:15:37 +0900

    Project show 화면 부모 프로젝트 링크 설정

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -60,7 +60,7 @@ class ProjectsController < ApplicationController
     @semester = DirectorySemester.find(@class.directory_semester_id)
     @project = Project.find(params[:id])
     @creator = User.find(@project.user_id)
-
+    @parent = Project.find(@project.parent_id) unless @project.parent_id.nil?
     # @content = CodeRay.scan(File.read('tmp/test.cpp'), :cpp).div
     @content = CodeRay.scan_file('tmp/test.cpp').div
 
