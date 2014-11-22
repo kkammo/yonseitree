86e6d62bbf4226dc30da0c0b368b71ea97b69dd9
kkammo <rland14@naver.com>
2014-11-22 23:40:23 +0900

    주석

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,22 +6,23 @@ class ApplicationController < ActionController::Base
   
   before_filter :require_permit, only: [:require_admin]
 
+  # 관리자 여부 확인
   def require_admin
     unless current_user && current_user.role == "admin"
       redirect_to root_path
-      # flash[:error] = "You are not an admin"
       flash[:admin_not] = "You are not an admin"
     end        
   end
 
+  # 유저의 로그인 여부 확인
   def require_user
     unless current_user
-      # redirect_to new_user_session_path
       redirect_to root_path
       flash[:sign_in] = "로그인이 필요합니다."
     end        
   end
 
+  # 유저의 승인 여부 확인
   def require_permit
     if current_user
       unless current_user.permit
@@ -30,14 +31,13 @@ class ApplicationController < ActionController::Base
         flash[:permit_not] = "관리자의 승인 이후 이용 가능합니다."
       end
     else
-      # redirect_to new_user_session_path
       redirect_to root_path
       flash[:sign_in] = "로그인이 필요합니다."
     end
   end
 
   protected
-
+# devise 파라미터
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:student_id, :role, :user_name, :email, :password, :password_confirmation, :remember_me) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :student_id, :email, :password, :remember_me) }
