424fd869be2ff8183685f670e2da8d8fa203f66f
kkammo <rland14@naver.com>
2014-11-22 22:40:08 +0900

    set env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.encoding = "utf-8"
+  config.time_zone = 'Seoul'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
