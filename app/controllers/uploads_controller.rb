a317ae504c06ef28b10876c705c7b7ad45d023d2
kkammo <rland14@naver.com>
2014-11-22 23:40:23 +0900

    주석

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -2,18 +2,23 @@ class UploadsController < ApplicationController
 	include Transloadit::Rails::ParamsDecoder
 	before_filter :require_permit
   
+  # 업로드 경로 생성
   def new
-  	@d_id = params[:dh_id]
+  	@d_id = params[:dh_id] #디렉토리 아이디 설정
   end
 
+  # 커밋시 업로드 경로 생성
   def commit
-  	@d_id = params[:directory_homework_id]
-  	@parent_id = params[:id]
+  	@d_id = params[:directory_homework_id] # 디렉토리 아이디 설정
+  	@parent_id = params[:id] # 상위 프로젝트 아이디 설정
   end
+
+  # 업로드 완료 후 이동
   def create
+    # 업로드 결과에 대한 정보를 로그로 남김
   	Rails.logger.info("PARAMS: #{params[:transloadit]}")
-  	@flash = params[:upload][:d_id]
-  	@parent = params[:upload][:parent_id]
+  	@flash = params[:upload][:d_id] # 디렉토리 아이디 설정
+  	@parent = params[:upload][:parent_id] # 상위 프로젝트 아이디 설정
   end
 
 end
