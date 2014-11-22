4e10a2a0c16eb4da72a51b98feffb751994f25d2
kkammo <rland14@naver.com>
2014-11-22 22:13:26 +0900

    fix error

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -89,11 +89,10 @@ class ProjectsController < ApplicationController
     load_directory_homework
 
     @project = @directory_homework.projects.new(project_params)
-  #  @project.project_id = params[:project][:parent_id]
-    @project.project_id = 6
+    @project.project_id = params[:project][:parent_id]
     @project.user_id = current_user.id
     @project.user_name = current_user.user_name
-    # @project.codefile = params[:project][:file_url]
+    @project.codefile = params[:project][:file_url]
 
     
     respond_to do |format|
